42ce2ff278b67887c903e8f452622863d3b1d104
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-24 13:21:03 +0100

    Implementierung der ersten Funktion

--- teil1/vehicle.c
+++ teil1/vehicle.c
@@ -1,1 +1,17 @@
+/*
+ * File: vehicle.c
+ * Beschreibung: Implementiert fahrzeugbezogene Funktionen
+ */
+
 #include "vehicle.h"
+#include <stdio.h>
+
+vehicle vehicle_create(int *p_next_id, int max_duration, int current_time)
+{
+    vehicle new_vehicle;
+    new_vehicle.id = 0;
+    new_vehicle.entry_time = 0;
+    new_vehicle.time_remaining = 0;
+
+    return new_vehicle;
+}
