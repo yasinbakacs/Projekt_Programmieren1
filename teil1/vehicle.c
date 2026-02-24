65b0d9c637a1604ca57bf11479e828b08a6612c8
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-24 14:04:37 +0100

    pseudocode hinzugefügt

--- teil1/vehicle.c
+++ teil1/vehicle.c
@@ -9,6 +9,26 @@
 vehicle vehicle_create(int *p_next_id, int max_duration, int current_time)
 {
     vehicle new_vehicle;
+
+    /*
+     * PSEUDOCODE:
+     *
+     * Falls p_next_id NULL ist:
+     *     spätere Fehlerbehandlung
+     *
+     * Falls max_duration <= 0:
+     *     spätere Fehlerbehandlung
+     * 
+     * new_vehicle.id = *p_next_id
+     * *p_next_id um 1 erhöhen
+     *
+     * new_vehicle.entry_time = current_time
+     * 
+     * new_vehicle.time_remaining = Zufallszahl im Bereich [1 ... max_duration]
+     *
+     * Fahrzeug zurückgeben
+     */
+
     new_vehicle.id = 0;
     new_vehicle.entry_time = 0;
     new_vehicle.time_remaining = 0;
