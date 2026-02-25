7d870815951cbbb17593108ac146febdd7efea3c
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-25 22:07:50 +0100

    kleine Kommentaränderungen und hinzufügen von queue_free Funktion

--- teil1/queue.h
+++ teil1/queue.h
@@ -5,7 +5,7 @@
 
 /*
  * Struktur: queue_node
- * Beschreibung: Ein Knoten der Warteschlange (dynamisch)
+ * Beschreibung: Ein Knoten der Warteschlange
  */
 typedef struct queue_node
 {
@@ -49,16 +49,16 @@ int queue_enqueue(queue *p_queue, vehicle new_item);
  *
  * @param[in,out] p_queue   Zeiger auf die Warteschlange
  * @param[out]    p_item    Zeiger auf Ausgabe-Fahrzeug
- * @return                  1 wenn ein Element entfernt wurde, sonst 0 (leer oder Fehler)
+ * @return                  1 wenn ein Fahrzeug entfernt wurde, sonst 0
  */
 int queue_dequeue(queue *p_queue, vehicle *p_item);
 
 
 /**
- * @brief Gibt die Anzahl der Elemente in der Warteschlange zurück
+ * @brief Gibt die Anzahl der Fahrzeuge in der Warteschlange zurück
  *
  * @param[in] p_queue Zeiger auf die Warteschlange
- * @return            Anzahl der Elemente
+ * @return            Anzahl der Fahrzeuge
  */
 int queue_get_size(const queue *p_queue);
 
@@ -71,5 +71,12 @@ int queue_get_size(const queue *p_queue);
  */
 int queue_is_empty(const queue *p_queue);
 
+
+/**
+ * @brief Gibt den Speicher der Warteschlange vollständig frei
+ *
+ * @param[in,out] p_queue Zeiger auf die Warteschlange
+ */
+void queue_free(queue *p_queue);
 
 #endif
