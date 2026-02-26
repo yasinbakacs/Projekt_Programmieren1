0512d654d0dd066084f26be1c59c66eece675fc0
Batu <oezdemir.batu-it25@it.dhbw-ravensburg.de>
2026-02-26 08:00:45 +0100

    Vollständige Simulation.c ohne Statistiken jedoch vorerst.

--- teil1/Simulation.c
+++ teil1/Simulation.c
@@ -54,3 +54,61 @@ void simulation_run(SimulationConfig config)
 
     // Am Ende: Gesamtauswertung ausgeben (noch nicht umgesetzt) 
 }
+
+/**
+ * @brief Führt einen einzelnen Zeitschritt aus.
+ */
+void simulation_step(SimulationConfig config, int step)
+{
+    // PSEUDOCODE:
+    // g_stats.current_step <- step
+    //
+    // Prüfen ob neues Fahrzeug ankommt:
+    // r <- RANDOM_NUMBER(0..99)
+    // IF r < config.arrival_probability THEN
+    //     Fahrzeug erzeugen (abhängig von vehicle.h)
+    //     v.id <- g_next_id
+    //     g_next_id <- g_next_id + 1
+    //     v.remaining_time <- RANDOM_NUMBER(1..config.max_parking_time)
+    //     v.entry_time <- step
+    //
+    //     Einparken oder warten?
+    //     free_index <- findFreeSpot(&g_garage)
+    //     IF free_index != -1 THEN
+    //         ok <- parkVehicle(&g_garage, v)
+    //         IF ok == true THEN
+    //             g_stats.cars_parked <- g_stats.cars_parked + 1
+    //         END IF
+    //     ELSE
+    //         QueueEnqueue(&g_queue, v)   // Name abhängig von queue.h
+    //     END IF
+    // END IF
+    //
+    // Abfahrten verarbeiten:
+    // departures_this_step <- 0
+    // processDepartures(&g_garage, &departures_this_step)
+    //
+    // Warteschlange nachrücken lassen:
+    // WHILE findFreeSpot(&g_garage) != -1 AND QueueIsEmpty(&g_queue) == false DO
+    //     next <- QueueDequeue(&g_queue)
+    //     ok <- parkVehicle(&g_garage, next)
+    // END WHILE
+
+    //Statistiken aktualisieren + ausgeben (noch nicht umgesetzt)
+}
+
+/**
+ * @brief Aktualisiert die Statistikwerte.
+ */
+void simulation_update_stats(SimulationStats *p_stats, ParkingGarage *p_garage, Queue *p_queue)
+{
+    // Statistiken aktualisieren
+}
+
+/**
+ * @brief Gibt Statistiken aus.
+ */
+void simulation_print_stats(SimulationStats stats)
+{
+    // Statistiken ausgeben
+} 
