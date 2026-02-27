b92ad8bbf908d0f7bcbd0736b7024857a5ad5af7
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-02-27 14:42:15 +0100

    read_int Hilfsfunktion hinzugefügt

--- teil1/main.c
+++ teil1/main.c
@@ -5,3 +5,35 @@
 
 #include <stdio.h>
 #include "simulation.h"
+
+
+static int read_int(const char *p_prompt, int *p_value)
+{
+    int result = 0;
+
+    if (p_prompt == NULL)
+    {
+        return 0;
+    }
+
+    if (p_value == NULL)
+    {
+        return 0;
+    }
+
+    printf("%s", p_prompt);
+
+    result = scanf("%d", p_value);
+    if (result != 1)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+
+int main(void)
+{
+    
+}
