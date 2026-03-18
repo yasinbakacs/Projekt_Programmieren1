a252f98df27b5b88d3220fcdb5a4fb7795ac9248
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-03-18 09:28:12 +0100

    Erstellung der Testnamen und erstellen der Ausführung der Tests

---
+++ tests/test_vehicle.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdio.h>
+#include "vehicle.h"
+
+void test_vehicle_create_basic()
+{
+
+}
+
+void test_vehicle_create_id()
+{
+
+}
+
+
+int main()
+{
+    test_vehicle_create_basic();
+    test_vehicle_create_id();
+
+    printf("Alle Vehicle Tests bestanden!\n");
+    
+    return 0;
+}
