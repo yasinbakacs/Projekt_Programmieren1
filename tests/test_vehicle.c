8b5747b46ea9467581258a226827591286f61ba2
Sefa <yilmaz.sefa-it25@it.dhbw-ravensburg.de>
2026-03-18 09:37:47 +0100

    Erste Testfunktion implementiert

--- tests/test_vehicle.c
+++ tests/test_vehicle.c
@@ -4,7 +4,16 @@
 
 void test_vehicle_create_basic()
 {
+    int next_id = 1;
+    int max_duration = 10;
+    int current_time = 5;
 
+    vehicle v = vehicle_create(&next_id, max_duration, current_time);
+
+    assert(v.id == 1);
+    assert(v.entry_time == current_time);
+    assert(v.time_remaining >= 1 && v.time_remaining <= max_duration);
+    assert(next_id == 2);
 }
 
 void test_vehicle_create_id()
@@ -19,6 +28,6 @@ int main()
     test_vehicle_create_id();
 
     printf("Alle Vehicle Tests bestanden!\n");
-    
+
     return 0;
 }
